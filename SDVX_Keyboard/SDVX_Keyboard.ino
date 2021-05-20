faa06e93502adce59a89b4b8fa0c9d695e486808
muskit <tranalex7@gmail.com>
2021-05-20 00:54:53 -0700

    code cleanup, addt'l comments, pinout change for my controller

--- SDVX_Keyboard/SDVX_Keyboard.ino
+++ SDVX_Keyboard/SDVX_Keyboard.ino
@@ -1,33 +1,34 @@
 /**
  * For use with a 32u4-based Arduino (Pro Micro, Leonardo, etc).
- * 
- * Refer to defines below for pinout.
+ * "Pro Micro" knock-offs use "Arduino Micro" for board firmware.
  */
 #define ENCODER_OPTIMIZE_INTERRUPTS
-#define KNOBS_USE_KEYBOARD
 #include <Encoder.h>
 #include <Keyboard.h>
 #include <Mouse.h>
 
-#define BT_A 4
-#define BT_B 5
-#define BT_C 8
-#define BT_D 7
-#define FX_R 10
-#define FX_L 9
-#define START 6
+// un-comment to use VOL output as keypress instead of mouse
+//#define KNOBS_USE_KEYBOARD
 
+//Pinout (Arudino pins); change to fit your setup
+#define BT_A 7
+#define BT_B 6
+#define BT_C 5
+#define BT_D 4
+#define FX_R 8
+#define FX_L 9
+#define START 16
 #define ENCODER_L0 3
 #define ENCODER_L1 2
-
 #define ENCODER_R0 0
 #define ENCODER_R1 1
 
 Encoder enc_r(ENCODER_R0, ENCODER_R1);
 Encoder enc_l(ENCODER_L0, ENCODER_L1);
 
-long count_l, count_r;
-
+// for Serial output funs
+long count_l = 0, count_r = 0;
+unsigned long i = 0;
 
 void setup() {
   pinMode(BT_A, INPUT_PULLUP);
@@ -40,12 +41,11 @@ void setup() {
 
   Mouse.begin();
   Keyboard.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
-//  count_l = enc_l.read();
-//  count_r = enc_r.read();
-
+  // Buttons //
   digitalRead(START) ? Keyboard.release(KEY_RETURN) : Keyboard.press(KEY_RETURN);
 
   !digitalRead(BT_A) ? Keyboard.press('d') : Keyboard.release('d');
@@ -55,15 +55,15 @@ void loop() {
 
   !digitalRead(FX_L) ? Keyboard.press('c') : Keyboard.release('c');
   !digitalRead(FX_R) ? Keyboard.press('m') : Keyboard.release('m');
-  
-  // LEFT ENCODER //
+
+  // Encoder (VOL) //
   int leftInit = enc_l.read();
   int rightInit = enc_r.read();
-  delay(3);
+  delay(1);
+  int leftDelta = enc_l.read() - leftInit;
+  int rightDelta = enc_r.read() - rightInit;
   
   #ifdef KNOBS_USE_KEYBOARD
-    int leftDelta = enc_l.read() - leftInit;
-    int rightDelta = enc_r.read() - rightInit;
     if(leftDelta != 0)
     {
       if(leftDelta < 0)
@@ -101,10 +101,13 @@ void loop() {
       Keyboard.release(';');
     }
   #else
-    Mouse.move(enc_l.read()-leftInit, 0, 0);
-    Mouse.move(0, enc_r.read()-rightInit, 0);
+    Mouse.move(leftDelta, 0, 0);
+    Mouse.move(0, rightDelta, 0);
   #endif
-  
   enc_l.write(0);
   enc_r.write(0);
+
+  count_l += leftDelta;
+  count_r += rightDelta;
+  Serial.println(count_l+count_r);
 }
